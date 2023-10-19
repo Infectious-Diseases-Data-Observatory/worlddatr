8809dae900d5750b1635c2d2d9e93a33067b521f
RhysPeploe <rhyspeploe1998@gmail.com>
2023-10-19 12:35:11 +0100

    Initial commit.

---
+++ R/DERIVE_AGE_YEARS.R
@@ -0,0 +1,36 @@
+#' @title Convert AGE to years.
+#'
+#' @description Convert the AGE of all subjects to years and change the AGEU to "YEARS".
+#'
+#' @param DATA Data frame containing the AGE and AGEU variables; typically the Demographics (DM) domain.
+#'
+#' @return Data frame with AGE and AGEU in years as opposed to the original values.
+#'
+#' @author Rhys Peploe
+#'
+#' @example
+#'
+DERIVE_AGE_YEARS = function(DATA){
+  for (i in seq(1, nrow(DATA), 1)){
+    if(is.na(DATA$AGEU[i])){
+      next
+    }
+    else if(DATA$AGEU[i] == "DAYS"){
+      DATA$AGE[i] = floor(DATA$AGE[i]/365.25)
+      DATA$AGEU[i] = "YEARS"
+    }
+    else if(DATA$AGEU[i] == "WEEKS"){
+      DATA$AGE[i] = floor(DATA$AGE[i]/52)
+      DATA$AGEU[i] = "YEARS"
+    }
+    else if(DATA$AGEU[i] == "MONTHS"){
+      DATA$AGE[i] = floor(DATA$AGE[i]/12)
+      DATA$AGEU[i] = "YEARS"
+    }
+    else{
+      DATA$AGE[i] = floor(DATA$AGE[i])
+      DATA$AGEU[i] = "YEARS"
+    }
+  }
+  return(DATA)
+}
