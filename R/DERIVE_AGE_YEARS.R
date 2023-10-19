fb0ba5c132896f0eb53aec733dcea379740f17ea
RhysPeploe <rhyspeploe1998@gmail.com>
2023-10-19 15:20:25 +0100

    Edits to description

--- R/DERIVE_AGE_YEARS.R
+++ R/DERIVE_AGE_YEARS.R
@@ -1,14 +1,17 @@
 #' @title Convert AGE to years.
 #'
-#' @description Convert the AGE of all subjects to years and change the AGEU to "YEARS".
+#' @description Convert the AGE of all subjects to years and change the AGEU to
+#'   "YEARS".
 #'
-#' @param DATA Data frame containing the AGE and AGEU variables; typically the Demographics (DM) domain.
+#' @param DATA Data frame containing the AGE and AGEU variables; typically the
+#'   Demographics (DM) domain.
 #'
-#' @return Data frame with AGE and AGEU in years as opposed to the original values.
+#' @return Data frame with AGE and AGEU in years as opposed to the original
+#'   values.
+#' @export
 #'
 #' @author Rhys Peploe
 #'
-#' @example
 #'
 DERIVE_AGE_YEARS = function(DATA){
   for (i in seq(1, nrow(DATA), 1)){
