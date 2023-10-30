b1c93d8b50aa2f0ef7cba7615785da421b6a6a15
RhysPeploe <rhyspeploe1998@gmail.com>
2023-10-30 17:21:30 +0000

    Additional packages to import

--- R/iddoverse-package.R
+++ R/iddoverse-package.R
@@ -2,9 +2,25 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom admiral compute_bmi
+#' @importFrom admiral convert_blanks_to_na
 #' @importFrom anthro anthro_zscores
 #' @importFrom dplyr filter
+#' @importFrom dplyr first
+#' @importFrom dplyr full_join
+#' @importFrom dplyr group_by
+#' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr relocate
 #' @importFrom dplyr rename
+#' @importFrom dplyr right_join
+#' @importFrom dplyr row_number
+#' @importFrom janitor clean_names
+#' @importFrom lubridate month
 #' @importFrom magrittr %>%
+#' @importFrom stringr str_replace_all
+#' @importFrom stringr str_to_upper
+#' @importFrom tidyr pivot_wider
+#' @importFrom tidyselect starts_with
 ## usethis namespace: end
 NULL
