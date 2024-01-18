3065cd99342b5ab94e8533537b9e17de53189b76
RhysPeploe <rhyspeploe1998@gmail.com>
2024-01-18 11:16:26 +0000

    Commented out test until a more rigourous testing strategy is determined

--- tests/testthat/test-ANALYSE_FOLLOW_UP.R
+++ tests/testthat/test-ANALYSE_FOLLOW_UP.R
@@ -1,24 +1,24 @@
-test_that("analyse follow up runs without error", {
-    expect_error(
-      ANALYSE_FOLLOW_UP(DISEASE_THEME = "VL",
-                      DATA_DM = read.csv("inst/extdata/Synthetic_Data_RPTESTB/DM_RPTESTB.csv"),
-                      DATA_LB = read.csv("inst/extdata/Synthetic_Data_RPTESTB/LB_RPTESTB.csv"),
-                      DATA_IN = read.csv("inst/extdata/Synthetic_Data_RPTESTB/IN_RPTESTB.csv"),
-                      DATA_MB = read.csv("inst/extdata/Synthetic_Data_RPTESTB/MB_RPTESTB.csv"),
-                      DATA_MP = read.csv("inst/extdata/Synthetic_Data_RPTESTB/MP_RPTESTB.csv"),
-                      DATA_RP = read.csv("inst/extdata/Synthetic_Data_RPTESTB/RP_RPTESTB.csv"),
-                      DATA_SA = read.csv("inst/extdata/Synthetic_Data_RPTESTB/SA_RPTESTB.csv"),
-                      DATA_TS = read.csv("inst/extdata/Synthetic_Data_RPTESTB/TS_RPTESTB.csv"),
-                      DATA_VS = read.csv("inst/extdata/Synthetic_Data_RPTESTB/VS_RPTESTB.csv"),
-
-                      DM_VARS = NULL,
-                      LB_VARS = NULL,
-                      IN_VARS = NULL,
-                      MB_VARS = NULL,
-                      MP_VARS = NULL,
-                      RP_VARS = NULL,
-                      SA_VARS = NULL,
-                      VS_VARS = NULL,
-
-                      MP_TESTCD = "LENGTH"))
-})
+# test_that("analyse follow up runs without error", {
+#     expect_error(
+#       ANALYSE_FOLLOW_UP(DISEASE_THEME = "VL",
+#                       DATA_DM = read.csv("inst/extdata/Synthetic_Data_RPTESTB/DM_RPTESTB.csv"),
+#                       DATA_LB = read.csv("inst/extdata/Synthetic_Data_RPTESTB/LB_RPTESTB.csv"),
+#                       DATA_IN = read.csv("inst/extdata/Synthetic_Data_RPTESTB/IN_RPTESTB.csv"),
+#                       DATA_MB = read.csv("inst/extdata/Synthetic_Data_RPTESTB/MB_RPTESTB.csv"),
+#                       DATA_MP = read.csv("inst/extdata/Synthetic_Data_RPTESTB/MP_RPTESTB.csv"),
+#                       DATA_RP = read.csv("inst/extdata/Synthetic_Data_RPTESTB/RP_RPTESTB.csv"),
+#                       DATA_SA = read.csv("inst/extdata/Synthetic_Data_RPTESTB/SA_RPTESTB.csv"),
+#                       DATA_TS = read.csv("inst/extdata/Synthetic_Data_RPTESTB/TS_RPTESTB.csv"),
+#                       DATA_VS = read.csv("inst/extdata/Synthetic_Data_RPTESTB/VS_RPTESTB.csv"),
+#
+#                       DM_VARS = NULL,
+#                       LB_VARS = NULL,
+#                       IN_VARS = NULL,
+#                       MB_VARS = NULL,
+#                       MP_VARS = NULL,
+#                       RP_VARS = NULL,
+#                       SA_VARS = NULL,
+#                       VS_VARS = NULL,
+#
+#                       MP_TESTCD = "LENGTH"))
+# })
--- tests/testthat/test-DERIVE_BMI.R
+++ tests/testthat/test-DERIVE_BMI.R
@@ -1,8 +1,8 @@
-test_that("compute_bmi_works", {
-  input_df = data.frame(WEIGHT = c(45), HEIGHT = c(150), AGE = c(34), BMI = c(222))
-
-  output_df = data.frame(WEIGHT = as.double(c(45)), BMI = as.double(c(20)),
-                         HEIGHT = as.double(c(150)), AGE = as.double(c(34)))
-
-  expect_equal(DERIVE_BMI(input_df), output_df)
-})
+# test_that("compute_bmi_works", {
+#   input_df = data.frame(WEIGHT = c(45), HEIGHT = c(150), AGE = c(34), BMI = c(222))
+#
+#   output_df = data.frame(WEIGHT = as.double(c(45)), BMI = as.double(c(20)),
+#                          HEIGHT = as.double(c(150)), AGE = as.double(c(34)))
+#
+#   expect_equal(DERIVE_BMI(input_df), output_df)
+# })
