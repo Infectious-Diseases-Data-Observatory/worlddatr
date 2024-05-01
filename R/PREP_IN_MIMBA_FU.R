34ede56cd30ea9e1b4306def82bbd3fa6f6bd757
RhysPeploe <rhyspeploe1998@gmail.com>
2024-05-01 18:44:08 +0100

    Moved elements around to ensure actions happened in correct order

--- R/PREP_IN_MIMBA_FU.R
+++ R/PREP_IN_MIMBA_FU.R
@@ -14,34 +14,36 @@ PREP_IN_MIMBA_FU = function(DATA_IN){
     "Phloroglucinol Quinine",
     "Paracetamol Quinine",
     "Paracetamol Phloroglucinol Artemether"
-    )) 
-  
+    ))
+
+  DATA_IN$INTRT <- str_replace_all(DATA_IN$INTRT, "Artemether/Lumefantrine Paracetamol", "Artemether-Lumefantrine")
+  DATA_IN$INTRT <- str_replace_all(DATA_IN$INTRT, "Artemether/Lumefantrine Phloroglucinol tablet", "Artemether-Lumefantrine")
+  DATA_IN$INTRT <- str_replace_all(DATA_IN$INTRT, "Quinine \\(oral\\)", "Quinine")
+  DATA_IN$INTRT <- str_replace_all(DATA_IN$INTRT, "Quinine Paracetamol", "Quinine")
+  DATA_IN$INTRT <- str_replace_all(DATA_IN$INTRT, "Paracetamol Quinine", "Quinine")
+  DATA_IN$INTRT <- str_replace_all(DATA_IN$INTRT, "Phloroglucinol QUININE", "Quinine")
+  DATA_IN$INTRT <- str_replace_all(DATA_IN$INTRT, "Paracetamol Phloroglucinol Artemether", "Artemether")
+
   DATA_IN <- DATA_IN %>%
-    convert_blanks_to_na() %>% 
-    filter(INTRT %in% IN_VARS) %>%
+    convert_blanks_to_na() %>%
     mutate(
       INPRESP = str_to_upper(.data$INPRESP),
-      INOCCUR = str_to_upper(.data$INOCCUR)
+      INOCCUR = str_to_upper(.data$INOCCUR),
+      INTRT = str_to_upper(INTRT)
     )
-  
+
   DATA_IN$INPRESP <- str_replace_all(DATA_IN$INPRESP, "TRUE", "Y")
   DATA_IN$INOCCUR <- str_replace_all(DATA_IN$INOCCUR, "TRUE", "Y")
   DATA_IN$INOCCUR <- str_replace_all(DATA_IN$INOCCUR, "FALSE", "N")
   DATA_IN$INOCCUR <- str_replace_all(DATA_IN$INOCCUR, "UNKNOWN", "U")
-  
-  DATA_IN$INTRT <- str_replace_all(DATA_IN$INTRT, "Artemether/Lumefantrine Paracetamol", "Artemether-Lumefantrine")
-  DATA_IN$INTRT <- str_replace_all(DATA_IN$INTRT, "Artemether/Lumefantrine Phloroglucinol tablet", "Artemether-Lumefantrine")
-  DATA_IN$INTRT <- str_replace_all(DATA_IN$INTRT, "Quinine (Oral)", "Quinine")
-  DATA_IN$INTRT <- str_replace_all(DATA_IN$INTRT, "Quinine Paracetamol", "Quinine")
-  DATA_IN$INTRT <- str_replace_all(DATA_IN$INTRT, "Phloroglucinol Quinine", "Quinine")
-  DATA_IN$INTRT <- str_replace_all(DATA_IN$INTRT, "Paracetamol Phloroglucinol Artemether", "Artemether")
-  
+
   if (any(is.na(DATA_IN$INPRESP) == TRUE)) {
     DATA_IN[which(is.na(DATA_IN$INPRESP)), "INPRESP"] <- "N"
     DATA_IN[which(DATA_IN$INPRESP == "N"), "INOCCUR"] <- "Y"
   }
-  
-  DATA <- DATA_IN %>% 
+
+  DATA <- DATA_IN %>%
+    filter(INTRT %in% IN_VARS) %>%
     group_by(STUDYID, USUBJID) %>%
     mutate(ANTIMAL_SEQ = row_number()) %>%
     pivot_wider(id_cols = c(STUDYID, USUBJID),
@@ -49,9 +51,9 @@ PREP_IN_MIMBA_FU = function(DATA_IN){
                 values_from = c(INTRT, INROUTE, INEVINTX, INSTDTC, INDUR),
                 names_vary = "slowest",
                 names_glue = "ANTIMALARIAL_{ANTIMAL_SEQ}_{.value}")
-  
+
   DATA <- DATA %>%
     clean_names(case = "all_caps")
-   
+
   return(DATA)
 }
