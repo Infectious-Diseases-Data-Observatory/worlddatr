053d549479f47696fb87abd4e9799273b30ad929
Rhys Peploe <rhys.peploe@iddo.org>
2024-11-11 15:58:47 +0000

    Init commit

---
+++ R/world_map.R
@@ -0,0 +1,25 @@
+#' Global Coordinates of Countries and Territories, with Country Codes and
+#' Income Group Data
+#'
+#' A dataset with coordinate data of the perimeter of each country/territory in
+#' the world, along with information on the country/territory ISO codes and income
+#' group as defined by the World Bank.
+#'
+#' Note that region refers to the area which the points create a perimeter for
+#'
+#' @format ## A data frame with 99,338 rows and 11 columns:
+#' \describe{
+#'    \item{long}{Longitude of point}
+#'    \item{lat}{Latitude of point}
+#'    \item{group}{Numerical identifer for the region}
+#'    \item{order}{Sequence of points within region}
+#'    \item{region}{Name of Country or Territory}
+#'    \item{subregion}{Name of subregion, if appropiate}
+#'    \item{alpha_3_code}{The three digit ISO code for the country/territory}
+#'    \item{alpha_2_code}{The two digit ISO code for the country/territory}
+#'    \item{income_group}{Income classification of the country/territory}
+#'    \item{country}{Name of country or territory according to ISO data}
+#'    \item{economy}{Name of country or territory according to the World Bank data}
+#' }
+#'
+"world_map"
