deb3f783e3e5a0305c6bd9cbe7b6d13dbe10a4c3
Rhys Peploe <rhys.peploe@iddo.org>
2024-11-15 15:34:28 +0000

    Additional variables

--- R/world_income.R
+++ R/world_income.R
@@ -5,11 +5,13 @@
 #'
 #' @format ## A data frame with 250 rows and 5 columns:
 #' \describe{
-#'    \item{country}{Name of country or territory}
-#'    \item{alpha_2_code}{The two digit ISO code for the country/territory}
 #'    \item{alpha_3_code}{The three digit ISO code for the country/territory}
+#'    \item{alpha_2_code}{The two digit ISO code for the country/territory}
+#'    \item{numeric}{The numeric ISO code for the country/territory}
+#'    \item{country}{Name of country or territory}
 #'    \item{economy}{Name of country or territory according to the World Bank data}
 #'    \item{income_group}{Income classification of the country/territory}
+#'    \item{redcap_number}{The numeric value for the country/territory in the IDDO REDCap codebook}
 #' }
 #'
 "world_income"
