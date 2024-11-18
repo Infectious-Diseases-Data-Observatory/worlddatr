e9874d93bdf65db8bc0fcffa058ea63a7db9c151
Rhys Peploe <rhys.peploe@iddo.org>
2024-11-18 10:05:18 +0000

    Init commit

---
+++ man/figures/Logo.R
@@ -0,0 +1,10 @@
+sticker(iddo_fig,
+        package = "worlddatr",
+        s_x = 1,
+        p_size = 20,
+        p_color = "#FFFFFF",
+        p_family = "sans",
+        p_fontface = "bold",
+        h_fill = "#435c6d",
+        h_color = "#e31b23",
+        filename = "figures/worlddatr_logo.png")
