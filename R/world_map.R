c7d0366f11490b7785cde1b90232a9ca1ad05a9c
Rhys Peploe <rhys.peploe@iddo.org>
2025-01-02 11:25:54 +0000

    Add when data was accessed

--- R/world_map.R
+++ R/world_map.R
@@ -5,7 +5,13 @@
 #' the world, along with information on the country/territory ISO codes and
 #' income group as defined by the World Bank.
 #'
-#' Note that region refers to the area which the points create a perimeter for
+#' Note that region refers to the area which the points create a perimeter for.
+#'
+#' Dates that data was accessed on:
+#'   - World Bank: 11/11/2024
+#'   - ISO:        18/06/2024
+#'   - REDCap:     15/11/2024
+#'   - map_data:   18/11/2024 (ggplot2 v3.4.3)
 #'
 #' @format ## A data frame with 99,338 rows and 11 columns:
 #' \describe{
