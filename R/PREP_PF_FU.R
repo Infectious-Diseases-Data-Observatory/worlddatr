93bc7d95548f79254dc4384b2850b4c9d38d0874
Rhys Peploe <rhys.peploe@iddo.org>
2025-02-13 17:14:56 +0000

    str_to_upper; commented out sections that are obsolete whilst PF output is under review by PF experts

--- R/PREP_PF_FU.R
+++ R/PREP_PF_FU.R
@@ -11,23 +11,23 @@
 PREP_PF_FU = function(DATA_PF, VARS = NULL){
   PF_VARS = c(str_to_upper(VARS))
 
-  DATA_PF = DATA_PF %>%
+  DATA = DATA_PF %>%
     convert_blanks_to_na() %>%
     filter(.data$PFGENTRG %in% PF_VARS) %>%
     mutate(
       # PFSTRES = as.character(.data$PFSTRESN),
-      PFSTRES = as.character(.data$PFSTRESC),
-      PFORRES = as.character(.data$PFORRES)
+      PFSTRES = str_to_upper(as.character(.data$PFSTRESC)),
+      PFORRES = str_to_upper(as.character(.data$PFORRES))
       # DAY = .data$PFDY,
       # PFUNITS = as.character(NA)
     )
 
-  DATA_EMPTY <- DATA_PF %>%
-    filter(is.na(.data$VISITDY) & is.na(.data$VISITNUM)) %>% # & is.na(.data$DAY)
-    DERIVE_EMPTY_TIME()
-
-  DATA <- DATA_PF %>%
-    left_join(DATA_EMPTY)
+  # DATA_EMPTY <- DATA_PF %>%
+  #   filter(is.na(.data$VISITDY) & is.na(.data$VISITNUM)) %>% # & is.na(.data$DAY)
+  #   DERIVE_EMPTY_TIME()
+  #
+  # DATA <- DATA_PF %>%
+  #   left_join(DATA_EMPTY)
 
   # DATA[which(is.na(DATA$PFSTRES)), "PFSTRES"] <-
   #   DATA[which(is.na(DATA$PFSTRES)), "PFSTRESC"]
@@ -39,34 +39,34 @@ PREP_PF_FU = function(DATA_PF, VARS = NULL){
   # DATA[which(is.na(DATA$PFSTRESC)), "PFUNITS"] <-
   #   DATA[which(is.na(DATA$PFSTRESC)), "PFORRESU"]
 
-  DATA = DATA %>%
-    group_by(USUBJID, PFGENTRG, VISITNUM, VISITDY, EMPTY_TIME) %>%
-    arrange(USUBJID, PFSTRESC) %>%
-    mutate(RESULT = str_c(unique(PFSTRES),collapse = "|"),
-           STRESULT = NA) %>%
-    ungroup()
+  # DATA = DATA %>%
+  #   group_by(USUBJID, PFGENTRG, VISITNUM, VISITDY, EMPTY_TIME) %>%
+  #   arrange(USUBJID, PFSTRESC) %>%
+  #   mutate(RESULT = str_c(unique(PFSTRES),collapse = "|"),
+  #          STRESULT = NA) %>%
+  #   ungroup()
 
-  DATA[which(DATA$RESULT == "MUTATION|WILD TYPE" & is.na(DATA$STRESULT)), "STRESULT"] = "MUTATION"
-  DATA[which(is.na(DATA$STRESULT)), "STRESULT"] = DATA[which(is.na(DATA$STRESULT)), "RESULT"]
+  # DATA[which(DATA$RESULT == "MUTATION|WILD TYPE" & is.na(DATA$STRESULT)), "STRESULT"] = "MUTATION"
+  # DATA[which(is.na(DATA$STRESULT)), "STRESULT"] = DATA[which(is.na(DATA$STRESULT)), "RESULT"]
 
   DATA <- DATA %>%
     pivot_wider(
       id_cols = c(
-        .data$STUDYID, .data$USUBJID, .data$VISITDY, .data$VISITNUM,
-        .data$EMPTY_TIME),    # .data$DAY,
+        .data$STUDYID, .data$USUBJID, .data$PARASITE),    # .data$DAY,
       names_from = c(PFGENRI, .data$PFGENTRG),
-      values_from = .data$STRESULT,
+      values_from = c(.data$PFSTRES, .data$PFRESCAT),
       values_fn = first,
-      names_glue = "{PFGENRI}_{PFGENTRG}",
+      names_glue = "{PFGENRI}_{PFGENTRG}_{.value}",
       names_sort = TRUE
     )
 
   colnames(DATA) <- gsub("KELCH 13_KELCH 13", "KELCH_13", colnames(DATA))
   colnames(DATA) <- gsub("KELCH 13_", "K13_", colnames(DATA))
-  # colnames(DATA) <- gsub("PFUNITS", "UNITS", colnames(DATA))
+  colnames(DATA) <- gsub("PFSTRES", "", colnames(DATA))
 
   DATA <- DATA %>%
-    clean_names(case = "all_caps")
+    clean_names(case = "all_caps") %>%
+    select(-c((ends_with("PFRESCAT") & starts_with("K13"))))
 
   return(DATA)
 }
