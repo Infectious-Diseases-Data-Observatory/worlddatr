5ec33754756c8288941ff7bcec43ef67e0869ef5
Rhys Peploe <rhyspeploe1998@gmail.com>
2026-03-09 13:30:43 +0000

    Move country_name_lookup into data

---
+++ R/country_name_lookup.R
@@ -0,0 +1,15 @@
+#' Country Names Data Bank
+#'
+#' A dataset containing various names of countries, along with the standardised
+#' ISO three and two letter codes.
+#'
+#' ISO codes correct as of 9th September 2025 (09/09/2025)
+#'
+#' @format ## A data frame with 250 rows and 5 columns:
+#' \describe{
+#'    \item{alpha_3_code}{The three digit ISO code for the country/territory}
+#'    \item{alpha_2_code}{The two digit ISO code for the country/territory}
+#'    \item{country_name}{Name of country or territory}
+#' }
+#'
+"country_name_lookup"
